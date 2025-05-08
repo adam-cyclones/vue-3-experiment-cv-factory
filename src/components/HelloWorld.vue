37e4bddb4a78d0e8ff7e1c02382dc42ea9e9382e
Adam Crockett <adam.crockett@bridge-u.com>
2025-05-08 09:21:48 +0100

    prototype the input so that I can add tags as skills

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,72 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
-</template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+<script setup>
+  import { ref, useTemplateRef } from 'vue';
+
+  const tagsInput = useTemplateRef();
+  const tagsUsed = ref([]);
+  const TAGS_INPUT_NAME = 'tags';
+  const CV_FILE_NAME = "cv_file";
+
+  /**
+   * Cannot use v-model because I need to do something more advanced with the form.
+  */
+  const handleSubmit = (e) => {
+    const elForm = e.target;
+    const form = new FormData(elForm);
+
+    const newValue = form.get(TAGS_INPUT_NAME);
+    tagsUsed.value.push(newValue);
+
+    elForm.reset();
   }
-}
 </script>
 
+<template>
+  <div class="hello">
+    <form @submit.prevent="handleSubmit">
+      <div>
+        <label class="block" :for="CV_FILE_NAME">Upload a CV</label>
+        <input :id="CV_FILE_NAME" type="file" accept=".md"/>
+      </div>
+      <div class="pt-2">
+        <label class="block" :for="TAGS_INPUT_NAME">Tags</label>
+        <input :id="TAGS_INPUT_NAME" :name="TAGS_INPUT_NAME" :ref="tagsInput" required class="form-control" type="text" placeholder="skills..."/>
+        <button type="addTag">Write</button>
+      </div>
+    </form>
+
+  </div>
+  <div class="tag-wrapper">
+    <div v-if="tagsUsed.length"></div>
+    <span class="tag" v-for="tag in tagsUsed" :key="tag">
+      {{tag}}
+    </span>
+  </div>
+</template>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+
+.tag-wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  gap: .5rem;
+  margin: 2rem auto;
+  max-width: 31.25rem;
+  width: 100%;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.tag {
+  background-color: beige;
+  border-radius: .25rem;
+  border: 1px solid whitesmoke;
+  padding: .5rem 1rem;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.block {
+  display: block;
 }
-a {
-  color: #42b983;
+
+.pt-2 {
+  padding-top: 2rem;
 }
 </style>
