8ad9cd9dca50851099083e5dd67f446114506157
Adam Crockett <adam.crockett@bridge-u.com>
2025-05-10 13:11:14 +0100

    chore: cleanup

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,86 @@
 
 <script setup>
-  import { ref, useTemplateRef } from 'vue';
+  import { ref, useTemplateRef, defineExpose } from 'vue';
 
-  const tagsInput = useTemplateRef();
+  const elTagsInput = useTemplateRef('elTagsInput');
+  const elJDTextarea = useTemplateRef('elJDTextarea')
   const tagsUsed = ref([]);
+  const formError = ref(null);
+
+  const JOB_DESCRIPTION_NAME = 'jd';
   const TAGS_INPUT_NAME = 'tags';
   const CV_FILE_NAME = "cv_file";
-
+  
+  const strings = {
+    startOverText: 'Start over',
+    clipboardButtonText: 'Load from clipboard',
+    uploadFileInstruction: 'Upload a CV',
+    fileHelpText: 'Only markdown is supported.',
+    labelJD: 'Job Description',
+    labelTags: 'Tags',
+    commonAdd: 'Add',
+    skillsHelpText: 'Add your skills to see them apear here..'
+  }
   /**
    * Cannot use v-model because I need to do something more advanced with the form.
   */
   const handleSubmit = (e) => {
+    const elInput = elTagsInput.value;
     const elForm = e.target;
-    const form = new FormData(elForm);
 
-    const newValue = form.get(TAGS_INPUT_NAME);
+    if (!elInput.checkValidity()) {
+      formError.value = 'Error, tags must not be empty'.
+      return;
+    }
+
+    // should always have a value
+    const newValue = elInput.value;
+
     tagsUsed.value.push(newValue);
-
     elForm.reset();
   }
+
+  const handlePasteJD = async () => {
+    const clipText = await navigator.clipboard.readText()
+    elJDTextarea.value.innerText = clipText
+  }
+
+  defineExpose({ strings });
 </script>
 
 <template>
-  <div class="hello">
-    <form @submit.prevent="handleSubmit">
+  <section>
+    <button type="button">{{strings.startOverText}}</button>
+  </section>
+  <div>
+    {{formError}}
+  </div>
+  <div class="main-wrapper">
+    <form novalidate @submit.prevent="handleSubmit">
       <div>
-        <label class="block" :for="CV_FILE_NAME">Upload a CV</label>
-        <input :id="CV_FILE_NAME" type="file" accept=".md"/>
+        <label class="block" :for="JOB_DESCRIPTION_NAME">{{strings.labelJD}}</label>
+        <textarea ref="elJDTextarea" rows="10" class="block w-100" :name="JOB_DESCRIPTION_NAME"></textarea>
+        
+        <button type="button" @click="handlePasteJD">{{strings.clipboardButtonText}}</button>
       </div>
-      <div class="pt-2">
-        <label class="block" :for="TAGS_INPUT_NAME">Tags</label>
-        <input :id="TAGS_INPUT_NAME" :name="TAGS_INPUT_NAME" :ref="tagsInput" required class="form-control" type="text" placeholder="skills..."/>
-        <button type="addTag">Write</button>
+      <div class="mt-2">
+        <label class="block" :for="CV_FILE_NAME">{{strings.uploadFileInstruction}}</label>
+        <input :id="CV_FILE_NAME" type="file" accept=".md"/>
+        <p>{{strings.fileHelpText}}</p>
+      </div>
+      <div class="mt-2">
+        <label class="block" :for="TAGS_INPUT_NAME">{{strings.labelTags}}</label>
+        <input :id="TAGS_INPUT_NAME" :name="TAGS_INPUT_NAME" ref="elTagsInput" required="required" class="form-control" type="text" placeholder="skills..."/>
+        <button type="submit">{{ strings.commonAdd }}</button>
       </div>
     </form>
 
   </div>
   <div class="tag-wrapper">
-    <div v-if="tagsUsed.length"></div>
-    <span class="tag" v-for="tag in tagsUsed" :key="tag">
+    <p class="w-100 text-center" v-if="!tagsUsed.length">{{strings.skillsHelpText}}</p>
+    <button type="button" class="tag" v-for="tag in tagsUsed" :key="tag">
       {{tag}}
-    </span>
+    </button>
   </div>
 </template>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -60,13 +100,32 @@
   border-radius: .25rem;
   border: 1px solid whitesmoke;
   padding: .5rem 1rem;
+  cursor: pointer;
 }
 
 .block {
   display: block;
 }
 
+.text-center {
+  text-align: center;
+  display: block;
+}
+
+.w-100 {
+  width: 100%;
+}
+
 .pt-2 {
   padding-top: 2rem;
+}
+
+.mt-2 {
+  margin-top: 2rem;
+}
+
+.main-wrapper {
+  max-width: 50rem;
+  margin: 0 auto;
 }
 </style>
